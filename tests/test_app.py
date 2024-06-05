9f008ecad87714ca76fd4d4afd05b34d8f10a8e8
fr.peixoto <fr.peixoto@outlook.com>
2024-06-05 18:11:13 -0300

    add test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,2 @@
-import unittest
-
-from hello import hello
-
-class Testes(unittest.TestCase):
-	def test_func(self):
-		self.assertEqual(hello(), "Teste da APi")
-
-if __name__ == '__main__':
-	unittest.main()
+def test_example():
+    assert True
